a4e058715709d6d7b41c58144a5ed4aacc7f6b65
impel1o <gamer.pvs@gmail.com>
2017-03-01 17:08:18 +0300

    Initial commit

---
+++ src/main/java/ScriptBase.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+import java.util.Random;
+
+/**
+ * Created by d.baskakov on 01.03.2017.
+ */
+public class ScriptBase {
+
+    private static final String user = "root";
+    private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/users9?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    private static Connection con;
+
+    public static void main(String[] args) {
+
+        String insert1000 = "INSERT INTO student VALUES(?,?,?,?,?)";
+        String testName = "TestName";
+
+        PreparedStatement preparedStatement = null;
+        Random r = new Random();
+        try {
+            con = DriverManager.getConnection(url, user, password);
+
+            for (int i = 1; i < 1001; i++) {
+                preparedStatement = con.prepareStatement(insert1000);
+                preparedStatement.setInt(1, i);
+                preparedStatement.setString(2, testName + i);
+                preparedStatement.setInt(3, (int) (Math.random() * 88));
+                preparedStatement.setBoolean(4,r.nextBoolean());
+                preparedStatement.setTimestamp(5, new Timestamp(System.currentTimeMillis()));
+                preparedStatement.execute();
+            }
+
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException se) {
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException se) {
+            }
+        }
+    }
+}
